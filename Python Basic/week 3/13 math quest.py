29a17bf31f581b8db72908b21a06815db3349e47
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2020-11-16 20:48:18 +0600

    Create 13 math quest.py
    math

---
+++ Python Basic/week 3/13 math quest.py
@@ -0,0 +1,5 @@
+n=int(input())
+for i in range(1,n): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print([0,1,22,333,4444,55555,666666,7777777,88888888,999999999][i])
+    
+    
