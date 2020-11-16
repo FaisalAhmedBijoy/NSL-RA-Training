6800b0b69d3ee8f6e5b49cab51c915585ae87332
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2020-11-16 17:06:10 +0600

    validation and find
    string validation and find

---
+++ Python Basic/week 3/10 string validator.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    s = str(input())
+    print (any( i.isalnum() for i in s))
+   
+    print(any(i.isalpha() for i in s))
+    print(any(i.isdigit() for i in s))
+    print(any(i.islower() for i in s))
+    print(any(i.isupper() for i in s))
+    
+    
