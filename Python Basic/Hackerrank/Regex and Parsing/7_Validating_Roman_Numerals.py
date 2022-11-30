8a2493fd941a8df9b3066046c6bb17847932bdd4
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2022-11-30 22:19:13 +0600

    regex change in validation roman numericals

--- Python Basic/Hackerrank/Regex and Parsing/7_Validating_Roman_Numerals.py
+++ Python Basic/Hackerrank/Regex and Parsing/7_Validating_Roman_Numerals.py
@@ -8,7 +8,7 @@ True
 
 """
 
-regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})?(XC|XL|L?X{0,3})?(IX|IV|V?I{0,3})?$"	# Do not delete 'r'.
+regex_pattern = r"^M{0,3}?(CM|CD|D?C{0,3})?(XC|XL|L?X{0,3})?(IX|IV|V?I{0,3})?$"	# Do not delete 'r'.
 
 import re
 print(str(bool(re.match(regex_pattern, input()))))
