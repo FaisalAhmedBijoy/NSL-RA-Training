fba66e00bc6427e3e9878cd6f9786087d5da46a3
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2022-12-06 23:38:49 +0600

    detect hmtl tags attributes values

---
+++ Python Basic/Hackerrank/Regex and Parsing/13_detect_html_tags_attributues_values.py
@@ -0,0 +1,38 @@
+"""
+9
+<head>
+<title>HTML</title>
+</head>
+<object type="application/x-flash" 
+  data="your-file.swf" 
+  width="0" height="0">
+  <!-- <param name="movie" value="your-file.swf" /> -->
+  <param name="quality" value="high"/>
+</object>
+Sample Output
+
+head
+title
+object
+-> type > application/x-flash
+-> data > your-file.swf
+-> width > 0
+-> height > 0
+param
+-> name > quality
+-> value > high
+
+"""
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        if (len(attrs) > 0):
+            for attr in attrs:
+                print('->', attr[0], '>', attr[1])
+
+parser = MyHTMLParser()
+n=int(input())
+for _ in range(n):
+    parser.feed(input())
